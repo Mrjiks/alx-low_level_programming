9c968a5ec3a147ec7dad202a74d9d6afa534b98a
Mrjiks <jiksdevops@gmail.com>
2022-12-18 02:33:02 -0800

    Add program 100-print_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -  calculate largest prime of 612852475143
+ *
+ * Return: Success Always
+ */
+
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int pf;
+
+	for (pf = 2; pf < x; py++)
+	{
+		if (n % pf == 0)
+		{
+			n = n / pf;
+		}
+	}
+	printf("%ld\n", pf);
+	return (0);
+}
